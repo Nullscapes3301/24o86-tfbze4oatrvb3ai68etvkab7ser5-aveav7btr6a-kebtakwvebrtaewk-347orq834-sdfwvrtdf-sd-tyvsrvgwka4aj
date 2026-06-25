33f4016f2d0d60a2ce549858640b8eb6843fd70d
Nullscapes3301 <contactwithnull@gmail.com>
2026-06-25 16:40:19 -0600

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,24 @@
+if self.actual_dir.startswith("putOS/home.dir/"):
+  pass
+else:
+  print("Acces denied.")
+class Extension:
+    name = "tree"
+
+    def run(self):
+        def show(node, prefix=""):
+            items = list(node.items())
+
+            for i, (name, value) in enumerate(items):
+                last = i == len(items) - 1
+                branch = "└── " if last else "├── "
+
+                print(prefix + branch + name)
+
+                if isinstance(value, dict):
+                    show(
+                        value,
+                        prefix + ("    " if last else "│   ")
+                    )
+
+        show(self.files)
