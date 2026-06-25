dfd66a76c8d0d273504dab4aaa6eb8fd0be65bb5
Nullscapes3301 <contactwithnull@gmail.com>
2026-06-24 21:30:59 -0600

    Create create.py

---
+++ create.py
@@ -0,0 +1,16 @@
+#putOS.cer.ext:create
+name = self.extarg1
+current = self.get_current()
+if " " in name:
+  print("Invalid name. Please use a valid name without spaces.")
+  return
+if name == "":
+  print("Invalid name. Please use a valid name without spaces.")
+  return
+elif name in current:
+  print("Already exists")
+elif name.endswith(".dir"):
+  current[name] = {}
+else:
+  self.file_content = ""
+  current[name] = self.file_content
