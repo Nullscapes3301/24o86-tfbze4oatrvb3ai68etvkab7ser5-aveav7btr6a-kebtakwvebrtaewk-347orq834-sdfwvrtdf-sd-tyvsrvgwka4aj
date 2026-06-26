32aa44ff978482b70146853c9b1398c1f72dc444
Nullscapes3301 <contactwithnull@gmail.com>
2026-06-26 16:30:00 -0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ if self.filecontent == "":
   arg1 = self.extarg1
   arg2 = self.extarg2
   arg3 = self.extarg3
-  print(eval(f"{arg1}{arg2}{arg3}"))
+  self.pr(eval(f"{arg1}{arg2}{arg3}"))
 else:
-  print(eval(self.filecontent))
+  self.pr(eval(self.filecontent))
