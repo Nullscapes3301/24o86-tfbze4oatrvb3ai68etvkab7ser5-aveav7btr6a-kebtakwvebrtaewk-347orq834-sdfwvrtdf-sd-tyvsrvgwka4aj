36b8625227a00e43306f0229a524ca88f39f1805
Nullscapes3301 <contactwithnull@gmail.com>
2026-06-27 10:43:19 -0600

    Update goto.py

--- goto.py
+++ goto.py
@@ -1,8 +1,10 @@
 if self.extarg1 == "save":
   self.goto = self.actual_dir
+  self.pr("Route saved")
 elif self.extarg1 == "show":
   self.pr(self.goto)
 elif self.extarg1 == "del":
   self.goto = ""
+  self.pr("Route deleted")
 else:
   self.actual_dir = self.goto
