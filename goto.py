534eb4520cfe20ef5e0b436d4e7afb7b99a18294
Nullscapes3301 <contactwithnull@gmail.com>
2026-06-27 10:52:58 -0600

    Implement error handling for goto variable access
    Added error handling for printing 'goto' variable.

--- goto.py
+++ goto.py
@@ -2,9 +2,15 @@ if self.extarg1 == "save":
   self.goto = self.actual_dir
   self.pr("Route saved")
 elif self.extarg1 == "show":
-  self.pr(self.goto)
+  try:
+    self.pr(self.goto)
+  except:
+    self.goto = ""
 elif self.extarg1 == "del":
   self.pr("Route deleted")
 else:
   if not self.goto == "":
-    self.actual_dir = self.goto
+      try:
+        self.pr(self.goto)
+      except:
+        self.goto = ""
