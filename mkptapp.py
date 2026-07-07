9eeca905308d8bb82a4c12dcca58aab310408c91
Nullscapes3301 <contactwithnull@gmail.com>
2026-07-07 08:52:43 -0600

    Refactor input handling in mkptapp.py

--- mkptapp.py
+++ mkptapp.py
@@ -2,7 +2,6 @@ def code(arg):
     import base64
     return base64.b64encode(arg.encode()).decode()
 
-while True:
-    i=self.inp("text > ")
-    i = i.replace("\\n", "\n")
-    self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
+i=self.inp("text > ")
+i = i.replace("\\n", "\n")
+self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
