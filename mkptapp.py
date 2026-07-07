bcdb166fb6807902d2c4c08a9295437708054c29
Nullscapes3301 <contactwithnull@gmail.com>
2026-07-07 08:51:41 -0600

    Implement a creator of putOS app python -> ptapp

---
+++ mkptapp.py
@@ -0,0 +1,8 @@
+def code(arg):
+    import base64
+    return base64.b64encode(arg.encode()).decode()
+
+while True:
+    i=self.inp("text > ")
+    i = i.replace("\\n", "\n")
+    self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
