9ac4304a773a64fe9985d3533f5f78da57efdd16
Nullscapes3301 <contactwithnull@gmail.com>
2026-07-07 09:25:51 -0600

    Fix syntax error in conditional statement

--- mkptapp.py
+++ mkptapp.py
@@ -2,7 +2,7 @@ def code(arg):
     import base64
     return base64.b64encode(arg.encode()).decode()
 
-if self.extarg1 = ".":
+if self.extarg1 == ".":
     i=self.inp("text > ")
     i = i.replace("\\n", "\n")
     self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
