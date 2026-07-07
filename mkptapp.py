7e4261687bb9f6f7e35ce445296c9bfe470fb4ef
Nullscapes3301 <contactwithnull@gmail.com>
2026-07-07 09:05:18 -0600

    Modify input processing for different extarg1 cases
    Refactor input handling based on extarg1 condition.

--- mkptapp.py
+++ mkptapp.py
@@ -2,6 +2,12 @@ def code(arg):
     import base64
     return base64.b64encode(arg.encode()).decode()
 
-i=self.inp("text > ")
-i = i.replace("\\n", "\n")
-self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
+if self.extarg1 = ".":
+    i=self.inp("text > ")
+    i = i.replace("\\n", "\n")
+    self.pr("putOS app > ", f"PTAPP{code(code(code(i)))}")
+else:
+    current = self.get_current()
+    i=self.inp("text > ")
+    i = i.replace("\\n", "\n")
+    current[self.extarg1] = f"PTAPP{code(code(code(i)))}"
