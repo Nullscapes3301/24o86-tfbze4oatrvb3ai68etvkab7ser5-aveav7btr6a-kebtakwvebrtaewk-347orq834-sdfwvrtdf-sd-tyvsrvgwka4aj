32568a138ed35dfd78e906e0865e13411ebf273f
Nullscapes3301 <contactwithnull@gmail.com>
2026-07-08 16:06:20 -0300

    Update mk.py

--- mk.py
+++ mk.py
@@ -1,19 +1,19 @@
 if self.user == "system":
-  pass
+  name = self.extarg1
+  current = self.get_current()
+  if "/" in name:
+    self.pr("Invalid name. Please use a valid name without '/'")
+  if " " in name:
+    self.pr("Invalid name. Please use a valid name without spaces.")
+  if name == "":
+    self.pr("Invalid name. Please use a valid name without spaces.")
+  elif name in current:
+    self.pr("Already exists")
+  elif name.endswith(".dir"):
+    current[name] = {}
+  else:
+    self.file_content = ""
+    current[name] = self.file_content
 else:
   self.pr("Access denied")
-name = self.extarg1
-current = self.get_current()
-if "/" in name:
-  self.pr("Invalid name. Please use a valid name without '/'")
-if " " in name:
-  self.pr("Invalid name. Please use a valid name without spaces.")
-if name == "":
-  self.pr("Invalid name. Please use a valid name without spaces.")
-elif name in current:
-  self.pr("Already exists")
-elif name.endswith(".dir"):
-  current[name] = {}
-else:
-  self.file_content = ""
-  current[name] = self.file_content
+
