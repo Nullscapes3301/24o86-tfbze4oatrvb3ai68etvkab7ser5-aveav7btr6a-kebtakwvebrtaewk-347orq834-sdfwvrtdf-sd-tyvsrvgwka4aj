4e27cbc1943b926e8f204f58aba4bb4f7dbd8d89
Nullscapes3301 <contactwithnull@gmail.com>
2026-08-31 16:56:10 -0600

    Modify eval calls to specify globals and locals
    Updated eval calls to include an empty dictionary for globals and locals.

--- calc.py
+++ calc.py
@@ -2,6 +2,6 @@ if self.filecontent == "":
   arg1 = self.extarg1
   arg2 = self.extarg2
   arg3 = self.extarg3
-  self.pr(eval(f"{arg1}{arg2}{arg3}"))
+  self.pr(eval(f"{arg1}{arg2}{arg3}"), {}, {})
 else:
-  self.pr(eval(self.filecontent))
+  self.pr(eval(self.filecontent), {}, {})
