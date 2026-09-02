13d8f3400ef6c483f7acc8c9c5a9b744b5ef9ab7
Nullscapes3301 <contactwithnull@gmail.com>
2026-09-01 18:48:02 -0600

    Fix condition check for wifimngr.dir existence

--- wifimngr.py
+++ wifimngr.py
@@ -9,7 +9,7 @@ if hasattr(self, "wifidata") and self.wifidata:
   pass
 else:
   self.wifidata = {}
-if "wifimngr.dir" in self.files["home.dir"]["AppData.dir"]["Local.dir"]
+if "wifimngr.dir" in self.files["home.dir"]["AppData.dir"]["Local.dir"]:
     self.wifidata = eval(self.files["home.dir"]["AppData.dir"]["Local.dir"]["wifimngr.dir"]["wifidata.wifi"])
 self.pr("putOS WiFi EZ manager")
 while True:
